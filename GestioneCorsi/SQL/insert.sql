fd72aa14708bf783d82cefd7c07275813d39bd5f
Betacom <Betacom@Betacom-PC7196.homenet.telecomitalia.it>
2021-11-29 16:09:49 +0100

    Immagini CV e file SQL

---
+++ GestioneCorsi/SQL/insert.sql
@@ -0,0 +1,14 @@
+insert into docente values('Benito', 'Bassano', 'CV1.png', 1);
+insert into docente values('Paolo', 'Rossi', 'CV2.png', 2);
+insert into docente values('Carmela', 'Neri', 'CV3.png', 3);
+insert into docente values('Mario', 'Rossi', 'CV4.png', 4);
+insert into docente values('Martina', 'Franchi', 'CV5.png', 5);
+insert into docente values('Ricardo', 'Fontana', 'CV6.png', 6);
+insert into docente values('Nicola', 'Antonutti', 'CV7.png', 7);
+insert into docente values('Claudia', 'Specchi', 'CV8.png', 8);
+
+insert into amministratore values('Massimo', 'Rossi', 'max', 'pass01');
+insert into amministratore values('Gianni', 'Verdi', 'gianni', 'pass02');
+insert into amministratore values('Laura', 'Bianchi', 'laura', 'pass03');
+insert into amministratore values('Dario', 'Arancio', 'dario', 'pass04');
+insert into amministratore values('Claudia', 'Viola', 'claudia', 'pass05');
