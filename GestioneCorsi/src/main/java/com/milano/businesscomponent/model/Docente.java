9d9b24cb9eb4e1eaf1cb8f16215f9d8bd9952382
Betacom <Betacom@Betacom-PC5792.homenet.telecomitalia.it>
2021-11-30 17:04:44 +0100

    Docente model

---
+++ GestioneCorsi/src/main/java/com/milano/businesscomponent/model/Docente.java
@@ -0,0 +1,41 @@
+package com.milano.businesscomponent.model;
+
+public class Docente {
+	private long codDocente;
+	private String nome;
+	private String cognome;
+	private String cvDocente;
+
+	public long getCodDocente() {
+		return codDocente;
+	}
+
+	public void setCodDocente(long codDocente) {
+		this.codDocente = codDocente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCvDocente() {
+		return cvDocente;
+	}
+
+	public void setCvDocente(String cvDocente) {
+		this.cvDocente = cvDocente;
+	}
+
+}
