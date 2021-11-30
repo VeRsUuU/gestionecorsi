098c6c65c8a356e8458110238754d3860df4ac1a
Betacom <Betacom@Betacom-PC051E.homenet.telecomitalia.it>
2021-11-30 16:48:54 +0100

    davide - creata classe CorsoCorsista 		- creata classe CorsoCorsistaDAO

---
+++ GestioneCorsi/src/main/java/com/milano/architecture/dao/CorsoCorsistaDAO.java
@@ -0,0 +1,14 @@
+package com.milano.architecture.dao;
+
+import java.util.ArrayList;
+
+import com.milano.businesscomponent.model.CorsoCorsista;
+
+public class CorsoCorsistaDAO {
+	
+	private ArrayList<CorsoCorsista> getAll(){
+		ArrayList<CorsoCorsista> lista = new ArrayList<CorsoCorsista>();
+		//TODO getall
+		return lista;
+	}
+}
---
+++ GestioneCorsi/src/main/java/com/milano/businesscomponent/model/CorsoCorsista.java
@@ -0,0 +1,30 @@
+package com.milano.businesscomponent.model;
+
+import java.util.Date;
+
+public class CorsoCorsista {
+	private long codCorso;
+	private long codCorsista;
+	private Date dataIscrizione;
+	
+	public long getCodCorso() {
+		return codCorso;
+	}
+	public void setCodCorso(long codCorso) {
+		this.codCorso = codCorso;
+	}
+	public long getCodCorsista() {
+		return codCorsista;
+	}
+	public void setCodCorsista(long codCorsista) {
+		this.codCorsista = codCorsista;
+	}
+	public Date getDataIscrizione() {
+		return dataIscrizione;
+	}
+	public void setDataIscrizione(Date dataIscrizione) {
+		this.dataIscrizione = dataIscrizione;
+	}
+	
+	
+}
