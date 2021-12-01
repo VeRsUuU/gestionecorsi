ee5221150e178f2c1625b3d3b964137d568bad2d
raijo <raijo@LAPTOP-FACN4TDA.station>
2021-12-01 10:16:51 +0100

    AdminBC

---
+++ GestioneCorsi/src/main/java/com/milano/businesscomponent/AdminBC.java
@@ -0,0 +1,40 @@
+package com.milano.businesscomponent;
+
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+import com.milano.architecture.dao.AdminDAO;
+import com.milano.architecture.dao.DAOException;
+import com.milano.architecture.dbaccess.DBAccess;
+import com.milano.businesscomponent.model.Admin;
+
+
+
+public class AdminBC {
+
+	private Connection conn;
+	
+	public AdminBC() throws ClassNotFoundException, DAOException, FileNotFoundException, IOException {
+		conn = DBAccess.getConnection();
+	}
+	
+	public Admin getById(Admin admin) throws DAOException {
+		try {
+			return AdminDAO.getFactory().getById(conn, admin);
+		}catch(SQLException exc) {
+			throw new DAOException(exc);
+		}finally {
+			DBAccess.closeConnection();
+		}
+	}
+	
+	
+	
+	
+	
+}
