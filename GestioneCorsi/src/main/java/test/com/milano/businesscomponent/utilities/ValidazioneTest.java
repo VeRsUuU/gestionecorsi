ec3ff846ab29976c61da813465edc4475dca3db4
Betacom <Betacom@Betacom-PC051E.homenet.telecomitalia.it>
2021-12-01 17:53:28 +0100

    VALIDATION

--- GestioneCorsi/src/main/java/test/com/milano/businesscomponent/utilities/ValidazioneTest.java
+++ GestioneCorsi/src/main/java/test/com/milano/businesscomponent/utilities/ValidazioneTest.java
@@ -22,14 +22,14 @@ class ValidazioneTest {
 	void test() throws DAOException {
 		HashMap<Integer, Date[]> mappa = new HashMap<Integer, Date[]>();
 		Date[] d = new Date[2];
-		GregorianCalendar g = new GregorianCalendar(2020,5,10);
+		GregorianCalendar g = new GregorianCalendar(2021,10,20);
 		d[0] = g.getTime();
-		GregorianCalendar g2 = new GregorianCalendar(2020,6,11);
-		d[1] = g.getTime();
+		GregorianCalendar g2 = new GregorianCalendar(2021,10,10);
+		d[1] = g2.getTime();
 		
 		mappa.put(1, d);
 		
-		HashMap<String, String> map = Validazione.valida("giovanni", "pierino", mappa);
+		HashMap<String, String> map = Validazione.valida("giovanni", "pierino2", mappa);
 		
 		for(Map.Entry m : map.entrySet())
 		    System.out.println(m.getKey()+" "+m.getValue());
