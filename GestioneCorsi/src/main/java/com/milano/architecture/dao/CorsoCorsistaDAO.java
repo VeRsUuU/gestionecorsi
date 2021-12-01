db6fd0de9d7b37b611191659b40dbcb874bdb5bf
Betacom <Betacom@Betacom-PC051E.homenet.telecomitalia.it>
2021-12-01 09:19:25 +0100

    NON TOCCARE DAOCONSTANTS

--- GestioneCorsi/src/main/java/com/milano/architecture/dao/CorsoCorsistaDAO.java
+++ GestioneCorsi/src/main/java/com/milano/architecture/dao/CorsoCorsistaDAO.java
@@ -1,18 +1,20 @@
 package com.milano.architecture.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.milano.businesscomponent.model.CorsoCorsista;
 
-public class CorsoCorsistaDAO {
+public class CorsoCorsistaDAO implements DAOConstants{
 	
 	private ArrayList<CorsoCorsista> getAll(Connection conn) throws SQLException{
 		ArrayList<CorsoCorsista> lista = new ArrayList<CorsoCorsista>();
 		
 		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(SELECT_CORSO_CORSISTA);
 		
 		
 		return lista;
