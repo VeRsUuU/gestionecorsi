94959357bf15f127b682915201095c305e1e8bfd
Betacom <Betacom@Betacom-PC7196.homenet.telecomitalia.it>
2021-12-01 16:32:54 +0100

    Creato DocenteBCTest e sistemato DocenteDAOTest

--- GestioneCorsi/src/main/java/test/com/milano/businesscomponent/DocenteBCTest.java
+++ GestioneCorsi/src/main/java/test/com/milano/businesscomponent/DocenteBCTest.java
@@ -1,72 +1,54 @@
-//creato da Zoia Simone
-
-package test.com.milano.architecture.dao;
+package test.com.milano.businesscomponent;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import com.milano.architecture.dao.DAOException;
-import com.milano.architecture.dao.DocenteDAO;
-import com.milano.architecture.dbaccess.DBAccess;
+import com.milano.businesscomponent.DocenteBC;
 import com.milano.businesscomponent.model.Docente;
 
 @TestMethodOrder(OrderAnnotation.class)
-class DocenteDAOTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		DBAccess.getConnection();
-	}
-
+class DocenteBCTest {
+	
 	@Test
 	@Order(1)
 	void testGetById() throws ClassNotFoundException, IOException {
 		try {
-			DocenteDAO dao = new DocenteDAO();
-			Docente doc = dao.getById(DBAccess.getConnection(), 8);
+			Docente doc = new Docente();
+			DocenteBC bc = new DocenteBC();
+			doc = bc.getById(8);
 			System.out.println("<--------Docente richiamato-------->");
 			System.out.println(doc.getCodDocente());
 			System.out.println(doc.getNomeDocente());
 			System.out.println(doc.getCognomeDocente());
 			System.out.println(doc.getCvDocente());
-		} catch (NullPointerException | DAOException exc) {
+		} catch(DAOException exc) {
 			exc.printStackTrace();
 			fail(exc.getMessage());
 		}
 	}
-
+	
 	@Test
 	@Order(2)
 	void testGetDocenteMaxCorsi() throws ClassNotFoundException, IOException {
 		try {
-			DocenteDAO dao = new DocenteDAO();
-			Docente doc = dao.getDocenteMaxCorsi(DBAccess.getConnection());
+			Docente doc = new Docente();
+			DocenteBC bc = new DocenteBC();
+			doc = bc.getDocenteMaxCorsi();
 			System.out.println("<--------Docente max corsi-------->");
 			System.out.println(doc.getCodDocente());
 			System.out.println(doc.getNomeDocente());
 			System.out.println(doc.getCognomeDocente());
 			System.out.println(doc.getCvDocente());
-		} catch (DAOException exc) {
+		} catch(DAOException exc) {
 			exc.printStackTrace();
 			fail(exc.getMessage());
-		}
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-		try {
-			DBAccess.closeConnection();
-		} catch (SQLException exc) {
-			throw new DAOException(exc);
 		}
 	}
 }
