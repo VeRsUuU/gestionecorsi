22f1ea2e4eb08cfcdf8433c96f59df0181010ae7
raijo <raijo@LAPTOP-FACN4TDA.station>
2021-12-01 09:47:06 +0100

    Admin Model

---
+++ GestioneCorsi/src/main/java/com/milano/businesscomponent/model/Admin.java
@@ -0,0 +1,45 @@
+package com.milano.businesscomponent.model;
+
+public class Admin {
+	
+	private String nomeAdmin;
+	private String cognomeAdmin;
+	private String password;
+	private String username;
+	
+	
+	public String getNomeAdmin() {
+		return nomeAdmin;
+	}
+	
+	public void setNomeAdmin(String nomeAdmin) {
+		this.nomeAdmin = nomeAdmin;
+	}
+	
+	public String getCognomeAdmin() {
+		return cognomeAdmin;
+	}
+	
+	public void setCognomeAdmin(String cognomeAdmin) {
+		this.cognomeAdmin = cognomeAdmin;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
