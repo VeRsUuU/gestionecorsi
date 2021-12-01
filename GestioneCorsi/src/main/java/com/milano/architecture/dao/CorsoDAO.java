cebaf9b489eb320354d35f3b235f8533ae4e6ae0
giuse <giuse@MSI>
2021-12-01 16:45:10 +0100

    Aggiunta classe test CorsoDAO. Implementazione metodo getCorsoPiuFrequentato in CorsoDAO

--- GestioneCorsi/src/main/java/com/milano/architecture/dao/CorsoDAO.java
+++ GestioneCorsi/src/main/java/com/milano/architecture/dao/CorsoDAO.java
@@ -1,15 +1,16 @@
 package com.milano.architecture.dao;
 
 import java.sql.Connection;
+
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.RowSetProvider;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import com.milano.businesscomponent.model.Corso;
 
@@ -17,21 +18,6 @@ public class CorsoDAO {
 
 	private final String SELECT_CORSI = "select * from corso";
 	private final String SELECT_CORSI_BYID = "select * from corso where cod_corso = ?";
-	private final String SELECT_CORSO_PIU_FREQUENTATO = "select t.*\r\n"
-			+ "from (select corso.*, count(corso_corsista.cod_corso) as iscritti\r\n"
-			+ "from corso, corso_corsista, docente, corsista\r\n"
-			+ "where corso.cod_corso = corso_corsista.cod_corso and corso.cod_docente = docente.cod_docente and corsista.cod_corsista = corso_corsista.cod_corsista\r\n"
-			+ "group by corso.cod_corso,corso.cod_docente,corso.nome_corso,corso.data_inizio,corso.data_fine,corso.commento,corso.aula_corso,corso.costo_corso) t";
-
-	private CachedRowSet rowSet;
-
-	public CorsoDAO() throws DAOException {
-		try {
-			rowSet = RowSetProvider.newFactory().createCachedRowSet();
-		} catch (SQLException sql) {
-			throw new DAOException(sql);
-		}
-	}
 
 	public ArrayList<Corso> getAll(Connection conn) throws DAOException {
 		ArrayList<Corso> corsi = null;
@@ -85,20 +71,18 @@ public class CorsoDAO {
 	public ArrayList<Corso> getCorsoPiuFrequentato(Connection conn) throws DAOException {
 		ArrayList<Corso> corsi = null;
 		try {
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(SELECT_CORSO_PIU_FREQUENTATO);
-			while (rs.next()) {
-				corsi = new ArrayList<Corso>();
-				Corso corso = new Corso();
-				corso.setCod(rs.getLong(1));
-				corso.setNome(rs.getString(3));
-				corso.setDataInizio(new Date(rs.getDate(4).getTime()));
-				corso.setDataFine(new Date(rs.getDate(5).getTime()));
-				corso.setCommenti(rs.getString(6));
-				corso.setAula(rs.getString(7));
-				corso.setCosto(rs.getDouble(8));
-				corsi.add(corso);
+			CorsoCorsistaDAO ccDao = new CorsoCorsistaDAO();
+			HashMap<Corso, Integer> prenotazioni = ccDao.getMap(conn);
+
+			int max = Collections.max(prenotazioni.values());
+
+			corsi = new ArrayList<Corso>();
+			for (Entry<Corso, Integer> entry : prenotazioni.entrySet()) {
+				if (entry.getValue() == max) {
+					corsi.add(entry.getKey());
+				}
 			}
+
 		} catch (SQLException sql) {
 			throw new DAOException(sql);
 		}
