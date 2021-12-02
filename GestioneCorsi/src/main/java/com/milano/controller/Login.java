4f5af65bfae6ae47973873965cda9241c46827ca
giuse <giuse@MSI>
2021-12-02 17:41:00 +0100

    modifica url

--- GestioneCorsi/src/main/java/com/milano/controller/Login.java
+++ GestioneCorsi/src/main/java/com/milano/controller/Login.java
@@ -39,7 +39,7 @@ public class Login extends HttpServlet {
 				if (userpass != null) {
 					if (userpass.equals(password)) {
 						session.setAttribute("username", username);
-						response.sendRedirect("riepilogocorsisti.jsp");
+						response.sendRedirect("riepilogoCorsisti.jsp");
 					} else {
 						session.setAttribute("errori", err+1);
 						response.sendRedirect("index.jsp");
