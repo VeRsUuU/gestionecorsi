597590e60d4ce5c38e0466c38dfe5c10acc4854c
Betacom <Betacom@Betacom-PC7196.homenet.telecomitalia.it>
2021-12-02 10:16:07 +0100

    aggiunta jsp accessonegato e aggiunto metodo getById(username) in admin

--- GestioneCorsi/src/main/java/com/milano/architecture/dao/AdminDAO.java
+++ GestioneCorsi/src/main/java/com/milano/architecture/dao/AdminDAO.java
@@ -6,9 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.milano.architecture.dao.DAOConstants;
-import com.milano.architecture.dao.DAOException;
-import com.milano.architecture.dbaccess.DBAccess;
 import com.milano.businesscomponent.model.Admin;
 
 public class AdminDAO implements DAOConstants {
@@ -17,15 +14,14 @@ public class AdminDAO implements DAOConstants {
 	 * 
 	 */
 
-	private String SELECT_ADMIN_BYUSERNAME = "select * from amministratore where username=?";
+	private String SELECT_ADMIN_BYUSERNAME = "select * from amministratore where username = ?";
 
-	// @Override// per implemento GEnerciDAO
+	// @Override// per implemento GenericDAO
 	public Admin getById(Connection conn, Admin model) throws DAOException, ClassNotFoundException, IOException {
 
 		Admin admin = null;
 		PreparedStatement ps;
 		try {
-
 			ps = conn.prepareStatement(SELECT_ADMIN_BYUSERNAME);
 			ps.setString(1, model.getUsername());
 
@@ -37,15 +33,30 @@ public class AdminDAO implements DAOConstants {
 				admin.setPassword(rs.getString(3));
 				admin.setUsername(rs.getString(4));
 				System.out.println("eseguito tutto");
-
 			}
-
-		} catch (SQLException sql) {
-			throw new DAOException(sql);
+		} catch (SQLException exc) {
+			throw new DAOException(exc);
 		}
-
 		return admin;
+	}
 
+	public Admin getById(Connection conn, String username) throws DAOException {
+		Admin admin = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_ADMIN_BYUSERNAME);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				admin = new Admin();
+				admin.setNomeAdmin(rs.getString(1));
+				admin.setCognomeAdmin(rs.getString(2));
+				admin.setUsername(rs.getString(3));
+			}
+		} catch (SQLException exc) {
+			throw new DAOException(exc);
+		}
+		return admin;
 	}
 
 }
--- GestioneCorsi/src/main/java/com/milano/businesscomponent/AdminBC.java
+++ GestioneCorsi/src/main/java/com/milano/businesscomponent/AdminBC.java
@@ -1,42 +1,39 @@
 package com.milano.businesscomponent;
 
-
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
-
 
 import com.milano.architecture.dao.AdminDAO;
 import com.milano.architecture.dao.DAOException;
 import com.milano.architecture.dbaccess.DBAccess;
 import com.milano.businesscomponent.model.Admin;
 
-
-
 public class AdminBC {
 	/*
-	 * create by
-	 * Raijon bida
+	 * create by Raijon bida
 	 * 
-	 * */
-	
-	
-	
+	 */
+
 	public Admin getById(Admin admin) throws DAOException, ClassNotFoundException, IOException {
-		AdminDAO a=new AdminDAO();
+		AdminDAO a = new AdminDAO();
 		try {
 			return a.getById(DBAccess.getConnection(), admin);
-		}catch(SQLException exc) {
+		} catch (SQLException exc) {
 			throw new DAOException(exc);
-		}finally {
+		} finally {
 			DBAccess.closeConnection();
 		}
 	}
-	
-	
-	
-	
-	
+
+	public Admin getById(String username) throws DAOException, ClassNotFoundException, IOException {
+		AdminDAO a = new AdminDAO();
+		try {
+			return a.getById(DBAccess.getConnection(), username);
+		} catch (SQLException exc) {
+			throw new DAOException(exc);
+		} finally {
+			DBAccess.closeConnection();
+		}
+	}
+
 }
